6341cc44aa9cc494ef46e895db0aa813524ec226
KgothatsoN <kg.otha@hotmail.com>
2021-01-18 17:13:52 +0000

    Added Form and Posts styles.js and envoc image

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import envoc from './images/envoc.png';
 
 const App = () => {
     return (
         <div>
-            <h1>App</h1>
+            <Container maxWidth="lg">
+                <AppBar  position="static" color="inherit">
+                    <Typography variant="h2" align="center">Envoc</Typography>
+                    <img src={envoc} alt="envoc" height="60"/>
+                </AppBar> 
+            </Container>      
         </div>
     )
 }
