fe063784c829c9d5bf71814d6da6cfcf4f8702ac
KgothatsoN <kg.otha@hotmail.com>
2021-01-18 17:17:41 +0000

    Styled Post and Form container grid

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,20 @@ const App = () => {
                 <AppBar  position="static" color="inherit">
                     <Typography variant="h2" align="center">Envoc</Typography>
                     <img src={envoc} alt="envoc" height="60"/>
-                </AppBar> 
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}>
+                                <Post/>
+
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                <Form/>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow> 
             </Container>      
         </div>
     )
