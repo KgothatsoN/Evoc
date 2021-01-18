30c2dcc0754f49bd3af4ad13f4feacd93d1f6c96
KgothatsoN <kg.otha@hotmail.com>
2021-01-18 16:19:26 +0000

    createPost function created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,29 @@
+import { request, response } from "express";
+import PostMsg from '../models/postMessage.js'
 
-export const getPosts = (request, response) => {
-    response.send('Working...');
+//retrieve posts from db
+export const getPosts = async (request, response) => {
+    try {
+        //Retried posts from data base
+        const postMsg = await PostMsg.find();
+
+        response.status(200).json(postMsg);
+    } catch (error) {
+        //client error with request post not found
+        response.status(404).json({message: error.message});
+    }
+}
+
+// create post function
+export const createPost = (request, response) => {
+    const post = request.body;
+    const newPost = new PostMsg(post);
+
+    try {
+        await newPost.save();
+        //successful request
+        response.status(201).json(newPost);
+    } catch (error) {
+        response.status(409).json({message: error.message})
+    }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getPosts} from '../controllers/posts.js'
+import {getPosts, createPost} from '../controllers/posts.js'
 
 // setup router
 const router = express.Router();
 
 //inital route
 router.get('/', getPosts);
+router.post('/', createPost);
 
 //export router to be used in index.js
 export default router;
